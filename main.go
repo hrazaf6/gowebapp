93e1428663ba3ef32311901cb9528b4fb14fd6bc
Hamid Raza <hrazaf6@gmail.com>
2020-02-28 15:11:33 +0530

    added check2

--- main.go
+++ main.go
@@ -17,10 +17,15 @@ func check1(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Health Check1</h1>")
 }
 
+func check2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Health Check2</h1>")
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/health_check", check)
 	http.HandleFunc("/health_check1", check1)
+	http.HandleFunc("/health_check2", check2)
 	fmt.Println("Server Starting...")
 	http.ListenAndServe(":3000", nil)
 }
